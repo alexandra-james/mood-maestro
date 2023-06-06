ad76896acb9d525d143402fa023cb0e60af86bc9
Alexandra James <alexandrajames@outlook.de>
2023-06-06 15:41:16 +0200

    created PlaylistSong model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_133422) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_133849) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "playlist_songs", force: :cascade do |t|
+    t.bigint "playlist_id", null: false
+    t.bigint "song_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["playlist_id"], name: "index_playlist_songs_on_playlist_id"
+    t.index ["song_id"], name: "index_playlist_songs_on_song_id"
+  end
 
   create_table "playlists", force: :cascade do |t|
     t.string "name"
@@ -48,5 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_133422) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "playlist_songs", "playlists"
+  add_foreign_key "playlist_songs", "songs"
   add_foreign_key "playlists", "users"
 end
