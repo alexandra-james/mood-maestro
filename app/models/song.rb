5136e47483079c3d7a5400bebd764df1bddc35ce
Alexandra James <alexandrajames@outlook.de>
2023-06-06 16:04:06 +0200

    added validations for all models

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ApplicationRecord
-  has_many :playlist_songs
+  has_many :playlist_songs, dependent: :destroy
+
+  validates :name, presence: true
+  validates :spotify_track_id, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :playlists, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true
 end
