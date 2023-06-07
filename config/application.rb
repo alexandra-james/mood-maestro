34c5d05f3ec9a29350197ca710b660427059f683
Elija Marx <elija.marx@gmail.com>
2023-06-07 10:57:12 +0200

    try to solve error in applications.rb

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module MoodMaestro
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    RSpotify.authenticate(ENV.fetch["CLIENT_ID"], ENV.fetch["CLIENT_SECRET"])
+    RSpotify.authenticate(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"])
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
