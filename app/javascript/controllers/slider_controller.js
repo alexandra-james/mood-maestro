5ea843292b5187b2b811d5302aa738536495d391
Elija Marx <elija.marx@gmail.com>
2023-06-14 14:32:58 +0200

    done with mode slider

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="slider"
 export default class extends Controller {
 
-  static targets = ["bpm"]
+  static targets = ["bpm", "mode"]
   connect() {
 
   }
@@ -12,5 +12,14 @@ export default class extends Controller {
     let value = event.target.value
     console.log(value)
     this.bpmTarget.innerText = `BPM ${value}`
+  }
+
+  mode(event) {
+    let value = event.target.value
+    if (value === "1") {
+      this.modeTarget.innerText = `Mode: Major`
+    } else {
+      this.modeTarget.innerText = `Mode: Minor`
+    }
   }
 }
