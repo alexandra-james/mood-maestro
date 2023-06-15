b422ea2de6b7ab082ce62cdf2b21b75015b481ad
Alexandra James <alexandrajames@outlook.de>
2023-06-15 11:26:38 +0200

    errors controller with custom views

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module MoodMaestro
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # errors
+    config.exceptions_app = self.routes
   end
 end
